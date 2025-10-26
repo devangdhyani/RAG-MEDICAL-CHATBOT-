f3abd8a07ff126c343e241c2838a1be269df451a
devang dhyani <dhyanidevang@example.com>
2025-10-26 12:36:30 +0530

    web app completed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Medical_Chatbot",
+    version="0.1.0",
+    author="devang dhyani",
+    author_email="dhyanidevang45@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
---
+++ src/prompt.py
@@ -0,0 +1,9 @@
+system_prompt=(
+    "you are an Medical assistant for question answering tasks."
+    "Use the following pieces of retrieved context to answer"
+    "the question,if u dont know the answer,say that you"
+    "don't know .Use three sentences maximum to keep the"
+    "answer concise."
+    "\n\n"
+    "{context}"
+)
